225f8b1fcde3b12f332cf48b3d13928d33cae5fe
Geoffrey Litt <gklitt@gmail.com>
2017-01-19 23:22:00 -0500

    playing with some ideas

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,22 @@
 class Book < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  recommend_based_on {
+    users: 1.0,
+    tags: 2.0
+  }
+
+  def self.recommended_for(user)
+    raw_recommendations = user.books.flat_map(&:similar_items)
+    Recommendation.squash(raw_recommendations)
+  end
+
+  def similar_items
+    # # Jaccard similarity
+    # SELECT
+    #   (# (array_agg(DISTINCT b1.user_id) & array_agg(DISTINCT b2.user_id)))::numeric /
+    #   (# (array_agg(DISTINCT b1.user_id) | array_agg(DISTINCT b2.user_id)))::numeric as similarity
+    # FROM books_users AS b1, books_users AS b2
+    # WHERE b1.book_id = 1002 AND b2.book_id = 1003;
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,44 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :books
+
+  # Returns an array of recommendations, each of which looks something like:
+  #  {
+  #    book123: {
+  #     score: ___,
+  #     reason: [
+  #       book234, book345
+  #     ]
+  #   }
+  # }
+  def recommended_books
+    Book.recommended_for(self) # puts all the recommendation at the item level
+  end
+end
+
+class Recommendation
+  attr_accessor :item, :score, :reason
+
+  def initialize(item, score, reason)
+    @item = item
+    @score = score
+    @reason = reason
+  end
+
+  # merges an array of recommendations with duplicate items into
+  # an array of one recommendation per item, with merged metadata
+  def self.squash(recommendations)
+    recommendations.each_with_object({}) do |recommendation, hash|
+      if hash[item]
+        hash[item] = hash[item].merge(recommendation)
+      else
+        hash[item] = recommendation
+      end
+    end
+  end
+
+  def merge(other)
+    raise ArgumentError unless self.item == other.item
+    self.score += other.score
+    self.reason << other.reason
+  end
 end
